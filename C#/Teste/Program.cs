f6076ae4a2082bd1921dd31c5afbe64c930ad46e
Kauan Marques Silva <kauanmqss@outlook.com>
2025-09-25 20:44:28 -0300

    Delete C#/Teste directory

--- C#/Teste/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Meu primeiro código em C#!");
-Console.WriteLine("--------------------------------------------------------");
-Console.WriteLine("Depois de anos, de volta à prática em c#!");
-
-Console.ReadKey();
